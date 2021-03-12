0e0642cb1495b13884ce247c098bb630d75a0091
xyzabhi <abhinavkumar8083@gmail.com>
2021-03-12 14:51:06 +0530

    Search component created succesfully

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 const items = [
   {
     title: "What is React?",
@@ -14,10 +15,11 @@ const items = [
   {
     title: "How do you use React?",
     content:
-      "You use React by creating components.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero. ",
+      "You use React by creating components.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero.",
   },
 ];
 const App = () => {
-  return <Accordion items={items} />;
+  return <Search />;
+  //   return <Accordion items={items} />;
 };
 export default App;
